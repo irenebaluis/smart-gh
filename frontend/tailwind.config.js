c066c6d06fd59e9d16045640e83cf61c46243686
Irene Baluis <irenecbaluis@gmail.com>
2023-04-29 23:16:54 +0800

    added frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // daisyUI config
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "emerald",
+  },
+}
+
